fb772ecd03abdda319553e9c4a2a27bb1f10918f
Derek Merkle <derekmerkle@gmail.com>
2019-02-13 13:28:05 -0500

    Add comic repo

--- Comics/Comics/Controllers/ComicBooksController.cs
+++ Comics/Comics/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
 ﻿using Comics.Models;
+using Comics.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,24 +10,16 @@ namespace Comics.Controllers
 {
     public class ComicBooksController : Controller
     {
+        private ComicBookRepository _comicRepository;
+
+        public ComicBooksController()
+        {
+            _comicRepository = new ComicBookRepository();
+        }
+
         public ActionResult Detail()
         {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Models.Artist() {Name = "Dan Slott", Role = "Script" },
-                    new Models.Artist() {Name = "Humberto Ramos", Role = "Pencils" },
-                    new Models.Artist() {Name = "Victor Olazaba", Role = "Inks" },
-                    new Models.Artist() {Name = "Edgar Delgado", Role = "Colors" },
-                    new Models.Artist() {Name = "Chris Eliopoulos", Role = "Letters" }
-                }
-            };
-
-            return View(comicBook);
+            return View(_comicRepository.GetComicBook(0));
         }
     }
 }
