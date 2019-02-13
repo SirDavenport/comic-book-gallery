97203daf2422ac49b0d1de1581c80a6165cf3d56
Derek Merkle <derekmerkle@gmail.com>
2019-02-13 12:01:04 -0500

    Changed the way we are passing the data from the controller.

--- Comics/Comics/Models/ComicBook.cs
+++ Comics/Comics/Models/ComicBook.cs
@@ -8,7 +8,26 @@ namespace Comics.Models
     public class ComicBook
     {
         public Artist[] Artists { get; set; }
+
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-")
+                    .ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
+
         public string DescriptionHtml { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
         public bool Favorite { get; set; }
         public int Id { get; set; }
         public int IssueNumber { get; set; }
