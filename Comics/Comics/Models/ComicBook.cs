5567a512fc0fdb0be4ed339b48e2c7f0591c7613
Derek Merkle <derekmerkle@gmail.com>
2019-02-13 11:46:30 -0500

    Added artist and comic book models

---
+++ Comics/Comics/Models/ComicBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Comics.Models
+{
+    public class ComicBook
+    {
+        public Artist[] Artists { get; set; }
+        public string DescriptionHtml { get; set; }
+        public bool Favorite { get; set; }
+        public int Id { get; set; }
+        public int IssueNumber { get; set; }
+        public string SeriesTitle { get; set; }
+    }
+}
