925b01abaff929b265a136788239e5a81ad5f0f1
Derek Merkle <derekmerkle@gmail.com>
2019-02-13 10:51:18 -0500

    Added ComicBooks view

--- Comics/Comics/Controllers/ComicBooksController.cs
+++ Comics/Comics/Controllers/ComicBooksController.cs
@@ -10,11 +10,7 @@ namespace Comics.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from the comic books controller");
+            return View();
         }
     }
 }
