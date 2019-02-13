063b498c2582bbfa297559ec3c064155f9bc89a3
Derek Merkle <derekmerkle@gmail.com>
2019-02-13 10:34:32 -0500

    Add method, detail, to ComicBooksController

--- Comics/Comics/Controllers/ComicBooksController.cs
+++ Comics/Comics/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace Comics.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the comic books controller";
+        }
     }
 }
